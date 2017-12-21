228aa889909782b125a624ea95c2ede444f53318
Christian Winter <christian.winter1990@gmail.com>
2017-12-21 22:30:17 +0100

    Add smoke particle sprites

--- game/player/player.gd
+++ game/player/player.gd
@@ -1,34 +1,34 @@
 extends "res://game/ship/baseship.gd"
 
 func _ready():
-	
+
 	properties[global.properties.movement_speed_forward] *= 1.2
 	properties[global.properties.ship_rotation_speed] *= 3
 	properties[global.properties.bullet_speed] *= 1.2
-	
+
 	fix_collision_shape()
 
 func initialize() :
-	
+
 	# add to group player
 	add_to_group(global.groups.player)
-	
+
 	# register to locator
 	get_node(global.scene_tree_ship_locator).register_ship(self)
-	
+
 	# call baseship init
 	.initialize()
-	
+
 func destroy(destroyer):
-	
+
 	var player_manager = get_node(global.scene_tree_player_manager)
-	
+
 	if(has_node("Processors/Input")) :
 		var input_processor = get_node("Processors/Input")
 		player_manager.unregister_device(input_processor.input_group, input_processor.device_id)
-	
+
 	.destroy(destroyer)
-	
+
 
 func reset_position():
 	var screensize = Vector2(Globals.get("display/width"), Globals.get("display/height"))
--- game/ship/baseship.gd
+++ game/ship/baseship.gd
@@ -24,14 +24,14 @@ func _ready():
 		global.properties.health_max: 1000,
 		global.properties.health: 1000
 	}
-	
+
 	initialize()
 	print("im ready baseship - ende")
 
 func initialize() :
 	set_fixed_process(true)
 	set_max_contacts_reported(4)
-	
+
 	set_mass(10)
 	set_weight(10)
 	set_friction(1)
@@ -39,22 +39,22 @@ func initialize() :
 	set_gravity_scale(0)
 	set_linear_damp(2)
 	set_angular_damp(4)
-	
+
 	set_max_contacts_reported(4)
-	
-	
-	
+
+
+
 	connect("body_enter", self, "processCollision")
-	
+
 	# Health bar
 	var health_scn = load(global.scene_path_healthbar)
 	var health_node = health_scn.instance()
 	#get_parent().add_child(health_node)
 	get_parent().call_deferred("add_child", health_node, true)
 	health_node.set_owner(self)
-	
+
 	reset_position()
-	
+
 # called to reset a position, usually after initialize
 func reset_position() :
 	pass
@@ -63,32 +63,32 @@ func reset_position() :
 # and . https://github.com/godotengine/godot/issues/8103
 func fix_collision_shape():
 	for shape in get_children():
-		if not shape extends CollisionShape2D and not shape extends CollisionPolygon2D: 
+		if not shape extends CollisionShape2D and not shape extends CollisionPolygon2D:
 			continue
 		if shape.has_meta("__registered") and shape.get_meta("__registered"):
 			continue
-		
+
 		get_tree().set_editor_hint(true)
-		
+
 		remove_child(shape) # Make it pick up the editor hint
 		add_child(shape)
-		
+
 		get_tree().set_editor_hint(false) # Unset quickly
-		
+
 		if shape extends CollisionShape2D: # Now update parent is working, so just change the shape
 			shape.set_shape(shape.get_shape())
 		elif shape extends CollisionPolygon2D:
 			shape.set_polygon(shape.get_polygon())
-		
+
 		remove_child(shape) # Reset its editor hint cache, just in case it was needed.. (you might drop this part if it bottlenecks)
 		add_child(shape)
-		
+
 		shape.set_meta("__registered", true)
 
 
 func handle_action(action, pressed):
 	get_node("moveable").handle_action(action, pressed)
-	
+
 func handle_mousemove(pos) :
 	get_node("moveable").handle_mousemove(pos)
 
@@ -117,17 +117,17 @@ func set_property(type, value):
 		properties[type] = value
 
 # empty implementation of shoot
-func shoot(path): 
-	
+func shoot(path):
+
 	#var shoot_scn = load("res://game/"+object+".tscn")
 	var shoot_scn = load(path)
 	var shoot_node = shoot_scn.instance()
 	#get_tree().get_current_scene().add_child(shoot_scn)
 	get_parent().add_child(shoot_node)
 	shoot_node.set_owner(self)
-	
+
 	#shoot_wait = get_property(global.properties.bullet_wait)
-	
+
 
 func can_destroy():
 	return true
@@ -141,10 +141,10 @@ func hit(power, hitter):
 	var health
 	health = get_property(global.properties.health) - power
 	set_property(global.properties.health, health)
-	
+
 	# update healthbar
 	#health_obj.changeHealth(-power);
-	
+
 	if (health <= 0):
 		destroy(hitter)
 		#get_node("anim").play("explode")
@@ -152,28 +152,28 @@ func hit(power, hitter):
 
 
 func processCollision(obstacle):
-	
+
 	if(obstacle.is_in_group('enemy') or obstacle.is_in_group('player')) :
 		var obstacle_vel = obstacle.get_linear_velocity()
 		var impact = get_linear_velocity().dot(obstacle_vel);
 		#print("processCollision ", obstacle.get_name(), obstacle_vel, get_linear_velocity(), impact)
 		#health_obj.changeHealth(-abs(impact) / 20000);
-		
-		#if health_obj.getHealth() <= 0: 
+
+		#if health_obj.getHealth() <= 0:
 		#	get_tree().change_scene(global.scene_path_gameover)
 		# process impact on obstacle
 		obstacle.processCollision(impact)
-		
+
 		# now about where we hit it
 		var player_pos = get_pos()
 		var obstacle_pos = obstacle.get_pos()
 		var hit_position = player_pos - obstacle_pos
 		print("hitpos: ", player_pos.normalized())
-		
+
 		"""
 		var raycast = RayCast(obstacle_pos)
-		
+
 		raycast.set_cast_to(player_pos)
-		if raycast.is_colliding() : 
+		if raycast.is_colliding() :
 			print("get_collision_point: ", raycast.get_collision_point(), " get_collider: ", raycast.get_collider())
 		"""
