3ef814a4185500c040ef175c4ddd2b8af3791e19
Christian Winter <christian.winter1990@gmail.com>
2018-02-16 00:01:11 +0100

    Add asteroid fragments. Happy birthday :)

---
+++ game/interfaces/spawner.gd
@@ -0,0 +1,21 @@
+extends Node
+
+onready var asteroid = get_parent()
+onready var destroyable = asteroid.get_node("destroyable")
+
+export(PackedScene) var left_part
+export(PackedScene) var right_part
+
+func _ready():
+	destroyable.connect("been_destroyed", self, "_on_been_destroyed")
+	
+func _on_been_destroyed(by_whom):
+	if left_part != null:
+		var part = left_part.instance()
+		get_tree().current_scene.add_child(part)
+		part.position = asteroid.position
+		
+	if right_part != null:
+		var part = right_part.instance()
+		get_tree().current_scene.add_child(part)
+		part.position = asteroid.position
